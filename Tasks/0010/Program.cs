e5e92a64767118a08660a186ccf6906e6ec53e9d
Realizeq <laptinov10@gmail.com>
2025-09-20 23:12:49 +0300

    Add files via upload

---
+++ Tasks/0010/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите ваше имя: ");
+string name = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(name) || name.Any(char.IsDigit))
+    Console.WriteLine("Введено не имя");
+else
+    Console.WriteLine(name);
---
+++ Tasks/0011/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите название команды: ");
+string teamName = Console.ReadLine();
+if (string.IsNullOrWhiteSpace(teamName) || teamName.Any(char.IsDigit))
+    Console.WriteLine("Введенное не является названием");
+else
+    Console.WriteLine($"{teamName} - это чемпион!");
