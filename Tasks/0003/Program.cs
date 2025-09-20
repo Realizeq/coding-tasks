d09344f95bfe4f37bfc13965409a1a2ae3b79f21
Realizeq <laptinov10@gmail.com>
2025-09-20 23:10:16 +0300

    Add files via upload

---
+++ Tasks/0003/Program.cs
@@ -0,0 +1,4 @@
+﻿int a = 50;
+int b = 10;
+Console.WriteLine(a);
+Console.WriteLine(b);
---
+++ Tasks/0004/Program.cs
@@ -0,0 +1,6 @@
+﻿int a = 5;
+int b = 10;
+int c = 21;
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
