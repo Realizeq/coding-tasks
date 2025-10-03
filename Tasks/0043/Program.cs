0003fa185c30cba34724377adfab2c634706bfa6
Realizeq <laptinov10@gmail.com>
2025-10-03 18:40:09 +0300

    Add files via upload

---
+++ Tasks/0043/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        // Первая точка
+        Console.WriteLine("Введите коорд. первой точки: ");
+        float x1 = Convert.ToSingle(Console.ReadLine());
+
+        Console.WriteLine("Введите коорд. первой точки: ");
+        float y1 = Convert.ToSingle(Console.ReadLine());
+
+        // Вторая точка
+        Console.WriteLine("Введите коорд. второй точки: ");
+        float x2 = Convert.ToSingle(Console.ReadLine());
+
+        Console.WriteLine("Введите коорд. второй точки: ");
+        float y2 = Convert.ToSingle(Console.ReadLine());
+
+        float diffX = x2 - x1;
+        float diffY = y2 - y1;
+        float dist = MathF.Sqrt(diffX * diffX + diffY * diffY);
+
+        Console.WriteLine($"Расстояние: {dist:f2}");
+    }
+}
