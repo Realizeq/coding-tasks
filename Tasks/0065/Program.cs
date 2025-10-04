6593cc73d3671ef7036356af6b7022d506a00a34
Realizeq <laptinov10@gmail.com>
2025-10-04 21:46:37 +0300

    Add files via upload

---
+++ Tasks/0065/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int k = 0;
+        int d = 0;
+
+        // Ввод и проверка дня года k
+        while (true)
+        {
+            Console.Write("Введите день года: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out k) && k >= 1 && k <= 365)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Введите число от 1 до 365.");
+            }
+        }
+
+        // Ввод и проверка дня недели 1 января d
+        while (true)
+        {
+            Console.Write("Введите день недели 1 января: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out d) && d >= 1 && d <= 7)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Введите число от 1 до 7.");
+            }
+        }
+
+        // Расчет дня недели для k
+        int n = (k + d - 2) % 7;
+
+        Console.WriteLine($"n = {n}");
+
+        // Вывод
+        Console.Write($"{k} день года приходится на ");
+        switch (n)
+        {
+            case 0:
+                Console.WriteLine("Воскресенье");
+                break;
+            case 1:
+                Console.WriteLine("Понедельник");
+                break;
+            case 2:
+                Console.WriteLine("Вторник");
+                break;
+            case 3:
+                Console.WriteLine("Среду");
+                break;
+            case 4:
+                Console.WriteLine("Четверг");
+                break;
+            case 5:
+                Console.WriteLine("Пятницу");
+                break;
+            case 6:
+                Console.WriteLine("Субботу");
+                break;
+        }
+    }
+}
---
+++ Tasks/0066/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = 0;
+
+        // Ввод и проверка количества месяцев
+        while (true)
+        {
+            Console.Write("Введите количество прошедших месяцев: ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out n) && n >= 0)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Ошибка! Введите неотрицательное целое число.");
+            }
+        }
+
+        // Расчет номера месяца
+        int x = (n % 12) + 1;
+
+        Console.WriteLine($"x = {x}");
+
+        // Вывод
+        Console.Write($"Месяц этого дня ");
+        switch (x)
+        {
+            case 1:
+                Console.WriteLine("Январь");
+                break;
+            case 2:
+                Console.WriteLine("Февраль");
+                break;
+            case 3:
+                Console.WriteLine("Март");
+                break;
+            case 4:
+                Console.WriteLine("Апрель");
+                break;
+            case 5:
+                Console.WriteLine("Май");
+                break;
+            case 6:
+                Console.WriteLine("Июнь");
+                break;
+            case 7:
+                Console.WriteLine("Июль");
+                break;
+            case 8:
+                Console.WriteLine("Август");
+                break;
+            case 9:
+                Console.WriteLine("Сентябрь");
+                break;
+            case 10:
+                Console.WriteLine("Октябрь");
+                break;
+            case 11:
+                Console.WriteLine("Ноябрь");
+                break;
+            case 12:
+                Console.WriteLine("Декабрь");
+                break;
+        }
+    }
+}
