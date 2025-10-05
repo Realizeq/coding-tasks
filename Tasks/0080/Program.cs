55bcc56b5dbd1c7d475ca35e8bea8d6674fb672f
Realizeq <laptinov10@gmail.com>
2025-10-05 20:47:52 +0300

    Add files via upload

---
+++ Tasks/0080/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.Write("Введите трехзначное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int units = number % 10;
+        int tens = (number / 10) % 10;
+        int hundreds = number / 100;
+
+        int newNumber = tens * 100 + hundreds * 10 + units;
+
+        Console.WriteLine($"Новое: {newNumber}");
+    }
+}
---
+++ Tasks/0081/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.Write("Введите трехзначное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int units = number % 10;
+        int tens = (number / 10) % 10;
+        int hundreds = number / 100;
+
+        int newNumber = hundreds * 100 + units * 10 + tens;
+
+        Console.WriteLine($"Новое: {newNumber}");
+    }
+}
