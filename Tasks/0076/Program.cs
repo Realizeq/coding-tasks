c7bbc7cb81ef4d1f34ab3b6cc58eaef06c907a25
Realizeq <laptinov10@gmail.com>
2025-10-05 20:46:34 +0300

    Add files via upload

---
+++ Tasks/0076/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.Write("Введите трехзначное число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int units = number % 10;
+        int tens = (number / 10) % 10;
+        int hundreds = number / 100;
+
+        int sum = units + tens + hundreds;
+        int product = units * tens * hundreds;
+
+        Console.WriteLine("а) Число единиц: " + units);
+        Console.WriteLine("б) Число десятков: " + tens);
+        Console.WriteLine("в) Сумма цифр: " + sum);
+        Console.WriteLine("г) Произведение цифр: " + product);
+    }
+}
