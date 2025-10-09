f838f3bd70e993130d51d5cf48a60b2a3cec604b
Realizeq <laptinov10@gmail.com>
2025-10-09 19:47:55 +0300

    Add files via upload

---
+++ Tasks/0095/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int n;
+
+        while (true)
+        {
+            Console.Write("Введите число n (100 < n < 999): ");
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out n) && n > 100 && n < 999)
+            {
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Введите число от 101 до 998.");
+            }
+        }
+
+        int a = n / 100;
+        int b = (n / 10) % 10;
+        int c = n % 10;
+
+        int x = a * 100 + b * 10 + c;
+
+        Console.WriteLine($"Исходное число x: {x}");
+    }
+}
---
+++ Tasks/0097/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int result = 654;
+
+        int a = result / 100;
+        int b = (result / 10) % 10;
+        int c = result % 10;
+
+        int x = c * 100 + a * 10 + b;
+
+        Console.WriteLine($"Исходное число x: {x}");
+    }
+}
