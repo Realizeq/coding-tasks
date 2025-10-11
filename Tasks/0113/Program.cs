05784c7a4939fd87aeee92e41dc14b9ff68c523f
Realizeq <laptinov10@gmail.com>
2025-10-11 21:55:58 +0300

    Add files via upload

---
+++ Tasks/0113/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        float x, y;
+
+        Console.Write("Введите координату x: ");
+        x = float.Parse(Console.ReadLine());
+
+        Console.Write("Введите координату y: ");
+        y = float.Parse(Console.ReadLine());
+
+        string region;
+
+        if (x > 0)
+        {
+            region = "I";
+        }
+        else
+        {
+            region = "II";
+        }
+
+        Console.WriteLine($"Точка находится в области: {region}");
+    }
+}
---
+++ Tasks/0114/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        float x;
+
+        Console.Write("Введите x: ");
+        x = float.Parse(Console.ReadLine());
+
+        float y;
+
+        if (x == 2)
+        {
+            y = 2;
+        }
+        else if (x == 3)
+        {
+            y = -3;
+        }
+        else
+        {
+            y = 0;
+        }
+
+        Console.WriteLine($"y = {y}");
+    }
+}
