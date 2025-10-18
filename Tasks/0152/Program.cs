a8a41f5c3cecb3d3923590121e8abf1343bc9eca
Realizeq <laptinov10@gmail.com>
2025-10-18 20:40:28 +0300

    Add files via upload

---
+++ Tasks/0152/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        Console.Write("Введите координату x: ");
+        float x = float.Parse(Console.ReadLine());
+        Console.Write("Введите координату y: ");
+        float y = float.Parse(Console.ReadLine());
+
+        if (x > 3 && y > 2)
+        {
+            Console.WriteLine("Точка попадает в область");
+        }
+        else
+        {
+            Console.WriteLine("Точка не попадает в область");
+        }
+    }
+}
---
+++ Tasks/0154/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите координату x: ");
+        float x = float.Parse(Console.ReadLine());
+
+        Console.Write("Введите координату y: ");
+        float y = float.Parse(Console.ReadLine());
+
+        if (y > 0)
+        {
+            if (x < -1)
+            {
+                Console.WriteLine("Точка попадает в область III");
+            }
+            else if (x > 5)
+            {
+                Console.WriteLine("Точка попадает в область I");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Точка не попадает в одну из областей");
+        }
+    }
+}
