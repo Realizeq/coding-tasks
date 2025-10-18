abd0776c32847e01199254b125f20ac8b1430370
Realizeq <laptinov10@gmail.com>
2025-10-18 15:55:42 +0300

    Add files via upload

---
+++ Tasks/0126/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите год рождения: ");
+        int birthYear = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите месяц рождения (1-12): ");
+        int birthMonth = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите текущий год: ");
+        int currentYear = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите текущий месяц (1-12): ");
+        int currentMonth = int.Parse(Console.ReadLine());
+
+        int age = currentYear - birthYear;
+
+        if (currentMonth < birthMonth)
+        {
+            age = age - 1;
+        }
+
+        Console.WriteLine($"Возраст: {age} полных лет");
+    }
+}
---
+++ Tasks/0127/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите площадь круга: ");
+        float circleArea = float.Parse(Console.ReadLine());
+
+        Console.Write("Введите площадь квадрата: ");
+        float squareArea = float.Parse(Console.ReadLine());
+
+        float circleRadius = (float)Math.Sqrt(circleArea / Math.PI);
+        float squareSide = (float)Math.Sqrt(squareArea);
+
+        bool circleInSquare = (2 * circleRadius) <= squareSide;
+        bool squareInCircle = (squareSide * (float)Math.Sqrt(2)) <= (2 * circleRadius);
+
+        Console.WriteLine($"а) Уместится ли круг в квадрате - {circleInSquare}");
+        Console.WriteLine($"б) Уместится ли квадрат в круге - {squareInCircle}");
+    }
+}
