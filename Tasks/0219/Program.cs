e2c66af260348b7a2fda0feaf3ce9865c3794f81
Realizeq <laptinov10@gmail.com>
2025-10-19 16:47:16 +0300

    Add files via upload

---
+++ Tasks/0219/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        float a = float.Parse(Console.ReadLine());
+
+        Console.Write("Введите второе число: ");
+        float b = float.Parse(Console.ReadLine());
+
+        Console.Write("Введите третье число: ");
+        float c = float.Parse(Console.ReadLine());
+
+        Console.Write("Введите четвертое число: ");
+        float d = float.Parse(Console.ReadLine());
+
+        int count = 0;
+
+        if (a < 0) count = count + 1;
+        if (b < 0) count = count + 1;
+        if (c < 0) count = count + 1;
+        if (d < 0) count = count + 1;
+
+        Console.WriteLine($"Количество отрицательных чисел: {count}");
+    }
+}
---
+++ Tasks/0221/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число a: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число b: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите число c: ");
+        int c = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+        if (a > 0) sum = sum + a;
+        if (b > 0) sum = sum + b;
+        if (c > 0) sum = sum + c;
+
+        Console.WriteLine($"Сумма положительных чисел: {sum}");
+    }
+}
