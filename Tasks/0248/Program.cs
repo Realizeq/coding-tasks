9e38a740f17a4ba621ddbfe09dfc65871150eced
Realizeq <laptinov10@gmail.com>
2025-10-20 17:41:21 +0300

    Add files via upload

---
+++ Tasks/0248/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите номер карты (6-14): ");
+        int k = int.Parse(Console.ReadLine());
+
+        string card;
+
+        switch (k)
+        {
+            case 6:
+                card = "шестерка";
+                break;
+            case 7:
+                card = "семерка";
+                break;
+            case 8:
+                card = "восьмерка";
+                break;
+            case 9:
+                card = "девятка";
+                break;
+            case 10:
+                card = "десятка";
+                break;
+            case 11:
+                card = "валет";
+                break;
+            case 12:
+                card = "дама";
+                break;
+            case 13:
+                card = "король";
+                break;
+            case 14:
+                card = "туз";
+                break;
+            default:
+                card = "ты не понимаешь что нет такого номера ?";
+                break;
+        }
+
+        Console.WriteLine(card);
+    }
+}
---
+++ Tasks/0250/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите номер дня года (1-365): ");
+        int k = int.Parse(Console.ReadLine());
+
+        // а) 1 января - понедельник
+        int dayOfWeekA = (k - 1) % 7;
+        string dayNameA;
+
+        switch (dayOfWeekA)
+        {
+            case 0: dayNameA = "понедельник"; break;
+            case 1: dayNameA = "вторник"; break;
+            case 2: dayNameA = "среда"; break;
+            case 3: dayNameA = "четверг"; break;
+            case 4: dayNameA = "пятница"; break;
+            case 5: dayNameA = "суббота"; break;
+            case 6: dayNameA = "воскресенье"; break;
+            default: dayNameA = "ещё раз попробуй давай"; break;
+        }
+
+        Console.WriteLine($"а) {k}-й день: {dayNameA}");
+
+        // б) 1 января - d день недели
+        Console.Write("Введите день недели 1 января (1-7): ");
+        int d = int.Parse(Console.ReadLine());
+
+        int dayOfWeekB = (k - 1 + d - 1) % 7;
+        string dayNameB;
+
+        switch (dayOfWeekB)
+        {
+            case 0: dayNameB = "понедельник"; break;
+            case 1: dayNameB = "вторник"; break;
+            case 2: dayNameB = "среда"; break;
+            case 3: dayNameB = "четверг"; break;
+            case 4: dayNameB = "пятница"; break;
+            case 5: dayNameB = "суббота"; break;
+            case 6: dayNameB = "воскресенье"; break;
+            default: dayNameB = "попутал ты"; break;
+        }
+
+        Console.WriteLine($"б) {k}-й день: {dayNameB}");
+    }
+}
