6c3d094ad7539c5796b9e7eb53c6084a824b2e5a
Realizeq <laptinov10@gmail.com>
2025-10-21 18:28:44 +0300

    Add files via upload

---
+++ Tasks/0272/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        for (int a = 2; a <= 17; a++)
+        {
+            float t = 3 * a;
+            float z = (2 - 430 * t) / (8 + 13);
+            Console.WriteLine($"a = {a,-3} z = {z:F2}");
+        }
+    }
+}
---
+++ Tasks/0274/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        for (float x = 0.1f; x <= 1.5f; x += 0.1f)
+        {
+            float sinValue = (float)Math.Sin(x);
+            Console.WriteLine($"sin {x:F1} = {sinValue:F2}");
+        }
+    }
+}
