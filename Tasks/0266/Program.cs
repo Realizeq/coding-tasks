62c12d6152224fd2b547bdd004802f449fbce9f3
Realizeq <laptinov10@gmail.com>
2025-10-21 18:26:40 +0300

    Add files via upload

---
+++ Tasks/0266/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        float p0 = 1.29f;
+        float a = 1.25e-4f;
+
+        Console.WriteLine("Высота  Плотность");
+
+        for (int h = 0; h <= 1000; h += 100)
+        {
+            float p = p0 * (float)Math.Exp(-a * h);
+            Console.WriteLine($"{h,-7} {p,-10:F4}");
+        }
+    }
+}
---
+++ Tasks/0267/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 9; i++)
+        {
+            int result = i * 7;
+            Console.WriteLine($"{i} x 7 = {result}");
+        }
+    }
+}
