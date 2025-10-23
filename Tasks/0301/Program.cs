9f4bcb9f1aac6b47df5b6a9dfa130452aeddd1d4
Realizeq <laptinov10@gmail.com>
2025-10-23 17:41:46 +0300

    Add files via upload

---
+++ Tasks/0301/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // а)
+        Console.Write("Введите k: ");
+        int k = int.Parse(Console.ReadLine());
+
+        int num1 = 1;
+        int den1 = 1;
+        int num2 = 2;
+        int den2 = 1;
+
+        if (k == 1)
+        {
+            Console.WriteLine($"а) {num1}/{den1}");
+        }
+        else if (k == 2)
+        {
+            Console.WriteLine($"а) {num2}/{den2}");
+        }
+        else
+        {
+            for (int i = 3; i <= k; i++)
+            {
+                int numNext = num1 + num2;
+                int denNext = den1 + den2;
+                num1 = num2;
+                den1 = den2;
+                num2 = numNext;
+                den2 = denNext;
+            }
+            Console.WriteLine($"а) {num2}/{den2}");
+        }
+
+        // б)
+        Console.Write("Введите n: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("б) ");
+        int numA = 1;
+        int denA = 1;
+        int numB = 2;
+        int denB = 1;
+
+        if (n >= 1) Console.Write($"{numA}/{denA} ");
+        if (n >= 2) Console.Write($"{numB}/{denB} ");
+
+        for (int i = 3; i <= n; i++)
+        {
+            int numNext = numA + numB;
+            int denNext = denA + denB;
+            Console.Write($"{numNext}/{denNext} ");
+            numA = numB;
+            denA = denB;
+            numB = numNext;
+            denB = denNext;
+        }
+    }
+}
---
+++ Tasks/0302/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите n (n >= 4): ");
+        int n = int.Parse(Console.ReadLine());
+
+        float v1 = 0.0f;
+        float v2 = 0.0f;
+        float v3 = 1.5f;
+
+        for (int i = 4; i <= n; i++)
+        {
+            float vNext = (i - 1.0f) / (i * i + 1) * v3 - v2 + v1;
+            v1 = v2;
+            v2 = v3;
+            v3 = vNext;
+        }
+
+        Console.WriteLine($"{v3:F4}");
+    }
+}
