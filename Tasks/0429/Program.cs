b6aa3dc92195771f4f25f7d3a40abe5e617103dd
Realizeq <laptinov10@gmail.com>
2025-11-01 16:34:47 +0300

    Add files via upload

---
+++ Tasks/0429/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            Console.Write($"введите число a{i}: ");
+            int a = int.Parse(Console.ReadLine());
+            sum += a;
+        }
+
+        Console.WriteLine($"сумма чисел: {sum}");
+    }
+}
---
+++ Tasks/0431/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        double perimetr = 0;
+
+        for (int i = 1; i <= 12; i++)
+        {
+            Console.Write($"введите длину стороны {i}: ");
+            double storona = double.Parse(Console.ReadLine());
+            perimetr += storona;
+        }
+
+        Console.WriteLine($"периметр 12-угольника: {perimetr}");
+    }
+}
