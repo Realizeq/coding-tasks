a9fdfa99b531239c70f450a17fecc85e51582b69
Realizeq <laptinov10@gmail.com>
2025-11-02 14:19:00 +0300

    Add files via upload

---
+++ Tasks/0439/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int proizvedenie = 1;
+
+        Console.Write("введите количество чисел: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= n; i++)
+        {
+            Console.Write($"введите число a{i}: ");
+            int a = int.Parse(Console.ReadLine());
+            proizvedenie *= a;
+        }
+
+        Console.WriteLine($"произведение чисел: {proizvedenie}");
+    }
+}
---
+++ Tasks/0440/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int proizvedenie = 1;
+
+        Console.WriteLine("вводите положительные числа");
+        Console.WriteLine("0 стоп");
+
+        while (true)
+        {
+            int num = int.Parse(Console.ReadLine());
+            if (num == 0) break;
+            if (num > 0) proizvedenie *= num;
+        }
+
+        Console.WriteLine($"произведение положительных чисел: {proizvedenie}");
+    }
+}
