40660db3a144396604e7fe4c3b42df62792a6cf1
Realizeq <laptinov10@gmail.com>
2025-11-08 11:21:14 +0300

    Add files via upload

---
+++ Tasks/0530/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        bool ordered = true;
+
+        Console.Write("количество учеников: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.Write("рост ученика 1: ");
+        int prev = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i <= n; i++)
+        {
+            Console.Write($"рост ученика {i}: ");
+            int current = int.Parse(Console.ReadLine());
+
+            if (current >= prev) ordered = false;
+
+            prev = current;
+        }
+
+        if (ordered) Console.WriteLine("да");
+        else Console.WriteLine("нет");
+    }
+}
