20bf2b0f786db54fd33f6b84ccb809a83e3004e6
Realizeq <laptinov10@gmail.com>
2025-11-12 21:08:26 +0300

    Add files via upload

---
+++ Tasks/0632/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Вариант 1 - ввод по строкам:");
+
+        int sum1 = 0;
+        for (int uchenik = 1; uchenik <= 12; uchenik++)
+        {
+            Console.WriteLine($"Ученик {uchenik}:");
+            Console.Write("Оценка по предмету 1: ");
+            int ocenka1 = int.Parse(Console.ReadLine());
+            Console.Write("Оценка по предмету 2: ");
+            int ocenka2 = int.Parse(Console.ReadLine());
+            Console.Write("Оценка по предмету 3: ");
+            int ocenka3 = int.Parse(Console.ReadLine());
+
+            int summaUchenika = ocenka1 + ocenka2 + ocenka3;
+            sum1 += summaUchenika;
+            Console.WriteLine($"Сумма оценок ученика {uchenik}: {summaUchenika}");
+        }
+        Console.WriteLine($"Общая сумма всех оценок: {sum1}");
+
+        Console.WriteLine("Вариант 2 - ввод по столбцам:");
+
+        int sum2 = 0;
+        for (int predmet = 1; predmet <= 3; predmet++)
+        {
+            Console.WriteLine($"Предмет {predmet}:");
+            for (int uchenik = 1; uchenik <= 12; uchenik++)
+            {
+                Console.Write($"Оценка ученика {uchenik}: ");
+                int ocenka = int.Parse(Console.ReadLine());
+                sum2 += ocenka;
+            }
+        }
+        Console.WriteLine($"Общая сумма всех оценок: {sum2}");
+    }
+}
---
+++ Tasks/0633/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[,] zarplata = new int[12, 3];
+
+        for (int rabotnik = 0; rabotnik < 12; rabotnik++)
+        {
+            Console.WriteLine($"Работник {rabotnik + 1}:");
+            for (int mesyac = 0; mesyac < 3; mesyac++)
+            {
+                Console.Write($"Зарплата за месяц {mesyac + 1}: ");
+                zarplata[rabotnik, mesyac] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        int obshayaSumma = 0;
+        for (int rabotnik = 0; rabotnik < 12; rabotnik++)
+        {
+            for (int mesyac = 0; mesyac < 3; mesyac++)
+            {
+                obshayaSumma += zarplata[rabotnik, mesyac];
+            }
+        }
+        Console.WriteLine($"Общая сумма за квартал: {obshayaSumma}");
+
+        Console.WriteLine("Зарплата каждого работника за квартал:");
+        for (int rabotnik = 0; rabotnik < 12; rabotnik++)
+        {
+            int summaRabotnika = 0;
+            for (int mesyac = 0; mesyac < 3; mesyac++)
+            {
+                summaRabotnika += zarplata[rabotnik, mesyac];
+            }
+            Console.WriteLine($"Работник {rabotnik + 1}: {summaRabotnika}");
+        }
+
+        Console.WriteLine("Общая зарплата за каждый месяц:");
+        for (int mesyac = 0; mesyac < 3; mesyac++)
+        {
+            int summaMesyaca = 0;
+            for (int rabotnik = 0; rabotnik < 12; rabotnik++)
+            {
+                summaMesyaca += zarplata[rabotnik, mesyac];
+            }
+            Console.WriteLine($"Месяц {mesyac + 1}: {summaMesyaca}");
+        }
+    }
+}
