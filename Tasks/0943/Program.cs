e8efec27f5a7fa227eb7775dab7d113c3e211fab
Realizeq <laptinov10@gmail.com>
2025-11-30 14:25:10 +0300

    Add files via upload

---
+++ Tasks/0943/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        float[] naselenie = { 144, 8, 67, 146, 38, 83, 10, 126, 11, 46, 9, 17, 5, 12, 35, 61, 25, 14, 7, 19, 22, 6, 4, 16, 13, 8, 11, 9 };
+        float[] ploshad = { 17, 9, 10, 10, 9, 9, 8, 9, 8, 6, 5, 5, 4, 4, 3, 3, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1 };
+
+        float vsegoNas = 0;
+
+        for (int i = 0; i < 28; i++)
+        {
+            if (ploshad[i] > 5)
+            {
+                vsegoNas += naselenie[i];
+            }
+        }
+
+        Console.WriteLine($"Общая численность государств с площадью >5 млн кв.км: {vsegoNas} млн человек");
+    }
+}
---
+++ Tasks/0947/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        float[] yanvar = { 2.5f, 0, 1.2f, 3.7f, 0.8f, 2.1f, 0, 4.3f, 1.5f, 0.7f, 3.2f, 0, 2.8f, 1.1f, 0.9f, 0, 2.4f, 3.9f, 1.8f, 0.6f, 2.7f, 0, 1.4f, 3.5f, 0.5f, 2.2f, 0, 4.1f, 1.7f, 0.8f, 2.9f };
+        float[] mart = { 1.8f, 2.3f, 0.9f, 3.1f, 1.2f, 2.7f, 0.5f, 3.8f, 1.1f, 2.4f, 0.7f, 3.3f, 1.5f, 2.9f, 0.8f, 3.5f, 1.3f, 2.6f, 0.6f, 3.7f, 1.4f, 2.8f, 0.9f, 3.2f, 1.6f, 2.5f, 0.7f, 3.9f, 1.2f, 2.1f, 0.8f };
+
+        float summYanvar = 0;
+        float summMart = 0;
+
+        for (int i = 0; i < 31; i++)
+        {
+            summYanvar += yanvar[i];
+            summMart += mart[i];
+        }
+
+        if (summYanvar > summMart)
+        {
+            Console.WriteLine($"В январе выпало больше осадков: {summYanvar} мм");
+        }
+        else if (summMart > summYanvar)
+        {
+            Console.WriteLine($"В марте выпало больше осадков: {summMart} мм");
+        }
+        else
+        {
+            Console.WriteLine($"Осадков выпало одинаково: {summYanvar} мм");
+        }
+    }
+}
