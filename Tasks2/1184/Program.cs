253e5fd33f5864e931944a8809245555d0ec2176
Realizeq <laptinov10@gmail.com>
2025-12-16 15:02:39 +0300

    Add files via upload

---
+++ Tasks2/1184/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        float[] osadki = {2.5f, 0, 1.2f, 3.7f, 0.8f, 2.1f, 0, 4.3f, 1.5f, 0.7f,
+                         3.2f, 0, 2.8f, 1.1f, 0.9f, 0, 2.4f, 3.9f, 1.8f, 0.6f,
+                         2.7f, 0, 1.4f, 3.5f, 0.5f, 2.2f, 0, 4.1f, 1.7f, 0.8f};
+
+        float[] temp = {5, -2, 3, -5, 2, -3, 1, -7, 4, -1,
+                       6, -4, 2, -2, 3, 0, 5, -6, 4, -1,
+                       3, -3, 2, -4, 1, -2, 4, -5, 3, -1};
+
+        float dozhd = 0;
+        float sneg = 0;
+
+        for (int i = 0; i < 30; i++)
+        {
+            if (temp[i] > 0)
+            {
+                dozhd += osadki[i];
+            }
+            else
+            {
+                sneg += osadki[i];
+            }
+        }
+
+        Console.WriteLine($"Осадков в виде дождя: {dozhd} мм");
+        Console.WriteLine($"Осадков в виде снега: {sneg} мм");
+    }
+}
---
+++ Tasks2/1186/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] vozrast = {25, 30, 35, 40, 28, 32, 45, 38, 27, 33,
+                        29, 31, 36, 42, 26, 34, 39, 41, 37, 44};
+
+        bool[] pol = {true, true, false, true, false, true, false, true, true, false,
+                     true, false, true, false, true, false, true, true, false, true};
+
+        float[] massa = {75, 82, 58, 85, 62, 78, 65, 80, 70, 60,
+                        77, 63, 79, 68, 72, 59, 81, 83, 64, 84};
+
+        float massaMuzhchin = 0;
+
+        for (int i = 0; i < 20; i++)
+        {
+            if (pol[i])
+            {
+                massaMuzhchin += massa[i];
+            }
+        }
+
+        Console.WriteLine($"Общая масса мужчин: {massaMuzhchin} кг");
+    }
+}
